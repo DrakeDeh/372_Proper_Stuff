c49560165e4e159b8c55222f4b5438c3c3f4b2c6
DrakeDeh <DehnertJJ27@uww.edu>
2025-03-08 13:13:36 -0600

    AddMoreOGDesign

---
+++ 372Fixed/Assets/Scripts/Design Scripts Plus/Basic Enemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    private static Logger Enemy;
+
+    private BasicEnemy()
+    {
+
+    }
+
+
+    public static Logger GetInstance()
+    {
+        if(Enemy == null)
+        {
+
+        }
+    }
+}
